65ee7e24043a1893b3c2af96d27075d590ca7ba5
KirbyIsSwag <57913656+YeetMeLol101@users.noreply.github.com>
2024-03-27 21:32:37 +0000

    Update Program.cs

--- Help/ConsoleApp1/Program.cs
+++ Help/ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@
 
 using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 
 namespace SkeletonProgramCS
@@ -71,7 +72,7 @@ namespace SkeletonProgramCS
                     //Finding all the text files in the directory
                     List<string> AllFileNames = new List<string>();
 
-                    DirectoryInfo d = new DirectoryInfo(@"C:\Users\ananmal.CRGS.000\Documents\Visual Studio 2022\Help\ConsoleApp1\bin\Debug\net7.0"); //Assuming Test is your Folder
+                    DirectoryInfo d = new DirectoryInfo(@"C:\Users\Anan Mallick\OneDrive\Documents\Visual Studio 2022\Morse-Code-Solver-main(1)\Morse-Code-Solver-main\Help\ConsoleApp1\bin\Debug\net7.0"); //Assuming Test is your Folder
 
                     FileInfo[] Files = d.GetFiles("*.txt"); //Getting Text files
                     string str = "";
@@ -255,6 +256,7 @@ namespace SkeletonProgramCS
 
         private static char Decode(string CodedLetter, int[] Dash, char[] Letter, int[] Dot)
         {
+            Console.WriteLine(CodedLetter);
             int CodedLetterLength = CodedLetter.Length;
             int Pointer = 0;
             char Symbol = SPACE;
@@ -313,9 +315,13 @@ namespace SkeletonProgramCS
                 {
                     Index = 0;
                 }
-                else
+                else if (Char.IsLetter(Convert.ToChar(PlainTextLetter)))
                 {
                     Index = (int)PlainTextLetter - (int)'A' + 1;
+                }
+                else
+                {
+                    Index = (int)PlainTextLetter - 45 + 27;
                 }
                 string CodedLetter = MorseCode[Index];
                 MorseCodeString = MorseCodeString + CodedLetter + SPACE;
@@ -323,13 +329,55 @@ namespace SkeletonProgramCS
             Console.WriteLine(MorseCodeString);
         }
 
-        private static void ConvertMorseCode(string[] Letter)
+        private static void ConvertMorseCode(int[] Dash, char[] Letter, int[] Dot)
         {
-            Console.Write("Enter your message (dots (.) and dashes (-) only");
-            string MorseText = Console.ReadLine();
-            int MorseTextLength = MorseText.Length;
+            string MorseCodeString = Console.ReadLine() + EOL;
+            int MorseCodeStringLength = MorseCodeString.Length;
+            char PlainTextLetter = SPACE;
+            string PlainText = EMPTYSTRING;
 
+            string SymbolString = EMPTYSTRING;
+            string CodedLetter = EMPTYSTRING;
+            char Symbol = SPACE;
+            bool LetterEnd = false;
+            for (int i = 0; i < MorseCodeStringLength; i++)
+            {
+
+                CodedLetter = EMPTYSTRING;
+                Symbol = SPACE;
+                LetterEnd = false;
+                while (!LetterEnd)
+                {
+                    Symbol = MorseCodeString[i];
+                    if (Symbol == SPACE)
+                    {
+                        LetterEnd = true;
+                        i += 4;
+                    }
+                    else if (MorseCodeString[i] == EOL)
+                    {
+                        
+                        LetterEnd = true;
+                    }
+                    else if (MorseCodeString[i + 1] == SPACE && MorseCodeString[i + 2] == SPACE)
+                    {
+                        LetterEnd = true;
+                        i += 3;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                    SymbolString = SymbolString + Symbol;
+                }
+                CodedLetter = CodedLetter + SymbolString;
+            }
+            Console.WriteLine(Decode(CodedLetter, Dash, Letter, Dot));
+            PlainTextLetter = Decode(CodedLetter, Dash, Letter, Dot);
+            PlainText = PlainText + PlainTextLetter;
+            Console.WriteLine(PlainTextLetter);
         }
+   
 
         private static void DisplayMenu()
         {
@@ -339,6 +387,8 @@ namespace SkeletonProgramCS
             Console.WriteLine("=========");
             Console.WriteLine("R - Receive Morse code");
             Console.WriteLine("S - Send Morse code");
+            Console.WriteLine("C - Translate Morse Code Directly");
+            Console.WriteLine("O - Output All Letters With Their Morse Code Counter Part");
             Console.WriteLine("X - Exit program");
             Console.WriteLine();
         }
@@ -354,12 +404,44 @@ namespace SkeletonProgramCS
             return MenuOption;
         }
 
+        private static void OutputAlphabetWithCode(char[] Letter, string[] MorseCode)
+        {
+            object[,] FourLetters = { {"", ""}, {"", ""}, {"", ""}, {"", ""} };
+            int ArrayCounter = 0;
+
+            int index = 0;
+            while (index < Letter.Length)
+            {
+                //FourLetters[ArrayCounter,0] = Alphabet[index];
+                //FourLetters[ArrayCounter, 1] = MorseCode[index];
+
+                FourLetters[ArrayCounter,0] = Letter[index];
+                FourLetters[ArrayCounter,1] = MorseCode[index];
+                index++;
+
+                if (ArrayCounter == 3)
+                {
+                    for (int k = 0; k < 4; k++)
+                    {
+                        Console.Write(FourLetters[k, 0] + "  " + FourLetters[k, 1] + "  ");
+                    }
+                    Console.WriteLine();
+                    ArrayCounter = 0;
+                }
+
+                
+                ArrayCounter++;
+            }
+            
+        }
+
         private static void SendReceiveMessages()
         {
-            int[] Dash = new int[] { 20, 23, 0, 0, 24, 1, 0, 17, 0, 21, 0, 25, 0, 15, 11, 0, 0, 0, 0, 22, 13, 0, 0, 10, 0, 0, 0 };
-            int[] Dot = new int[] { 5, 18, 0, 0, 2, 9, 0, 26, 0, 19, 0, 3, 0, 7, 4, 0, 0, 0, 12, 8, 14, 6, 0, 16, 0, 0, 0 };
-            char[] Letter = new char[] { ' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-            string[] MorseCode = new string[] { " ", ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+            
+            int[] Dash = new int[] { 20, 23, 0, 0, 24, 1, 0, 17, 34, 21, 31, 25, 0, 15, 11, 27, 0, 0, 0, 22, 13, 29, 33, 10, 0, 0, 0 ,30, 0, 32, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            int[] Dot = new int[]  { 5, 18, 36, 0, 2, 9, 0, 26, 35, 19, 0, 3, 0, 7, 4, 28, 0, 0, 12, 8, 14, 6, 0, 16, 0, 0, 0, 37, 39, 38, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            char[] Letter = new char[] { ' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+            string[] MorseCode = new string[] { " ", ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "----", "---.", "..--", "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."};
             bool ProgramEnd = false;
             string MenuOption = EMPTYSTRING;
             while (!ProgramEnd)
@@ -376,7 +458,11 @@ namespace SkeletonProgramCS
                 }
                 else if (MenuOption == "C")
                 {
-                    ConvertMorseCode(Letter)
+                    ConvertMorseCode(Dash, Letter, Dot);
+                }
+                else if (MenuOption == "O")
+                {
+                    OutputAlphabetWithCode(Letter, MorseCode);
                 }
                 else if (MenuOption == "X")
                 {
